6541b1c5b79df4d40bf1f5f1c4fcc97ea69783a7
viet6cltt <phanvanviet70@gmail.com>
2026-03-05 23:31:36 +0700

    init project

---
+++ src/main/java/org/example/week2/controller/UserController.java
@@ -0,0 +1,17 @@
+package org.example.week2.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Request processed successfully!");
+    }
+}
+
