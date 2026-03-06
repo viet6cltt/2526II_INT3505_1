bab5d057daf0c6337697eecad6141645cb05d423
viet6cltt <phanvanviet70@gmail.com>
2026-03-06 08:54:54 +0700

    feat: uniform interface

--- week2_onclass/server.py
+++ week2_onclass/server.py
@@ -8,16 +8,47 @@ students = [
     {"id": 3, "name": "Cuong"}
 ]
 
+# GET all students
 @app.route("/students", methods=["GET"])
 def get_students():
-    return jsonify(students)
+    return jsonify(students), 200
 
+
+# GET one student
 @app.route("/students/<int:id>", methods=["GET"])
 def get_student(id):
     for s in students:
         if s["id"] == id:
-            return jsonify(s)
-    return {"error": "Student not found"}, 404
+            return jsonify(s), 200
+    return {"message": "Student not found"}, 404
+
+
+# CREATE student
+@app.route("/students", methods=["POST"])
+def add_student():
+    data = request.json
+    students.append(data)
+    return jsonify(data), 201
+
+
+# UPDATE student
+@app.route("/students/<int:id>", methods=["PUT"])
+def update_student(id):
+    data = request.json
+    for s in students:
+        if s["id"] == id:
+            s["name"] = data["name"]
+            return jsonify(s), 200
+    return {"message": "Student not found"}, 404
+
+
+# DELETE student
+@app.route("/students/<int:id>", methods=["DELETE"])
+def delete_student(id):
+    global students
+    students = [s for s in students if s["id"] != id]
+    return {"message": "Deleted"}, 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
