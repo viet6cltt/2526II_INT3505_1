f6e11c187d72d43b3901629aad078c5cda7989a4
viet6cltt <phanvanviet70@gmail.com>
2026-03-20 07:55:10 +0700

    feat(week4): add vercel config

--- week4/server.py
+++ week4/server.py
@@ -16,5 +16,5 @@ swagger_ui_blueprint = get_swaggerui_blueprint(
 
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
-if __name__ == "__main__":
-  	app.run(debug=True)
+# if __name__ == "__main__":
+#   	app.run(debug=True)
